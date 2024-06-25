788477d6cae9d8bdf846ee72fc0162a91c548328
RohanJena <rohan.jena.eee22@itbhu.ac.in>
2024-06-25 21:50:59 +0530

    Agri_Loiter mode added

---
+++ build/sitl/ap_version.h
@@ -0,0 +1,11 @@
+// auto-generated header, do not edit
+
+#pragma once
+
+#ifndef FORCE_VERSION_H_INCLUDE
+#error ap_version.h should never be included directly. You probably want to include AP_Common/AP_FWVersion.h
+#endif
+
+#define GIT_VERSION "b841f551"
+#define GIT_VERSION_INT 3091330385
+#define AP_BUILD_ROOT "/home/rohan/ardu_dev/ardupilot"
